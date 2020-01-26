c951c4cd9f8fafc8de588fc48587568e210ecde1
MediocreFred <mediocrefred@users.noreply.github.com>
2020-01-26 18:42:59 -0500

    made it so the url function returns a tuple of team name and url

--- scrape.py
+++ scrape.py
@@ -5,15 +5,16 @@ import json
 def get_smjhl_players(url_file):
     """use the urls provided in the json to get each player's information"""
     url_list = get_roster_url(url_file, 'SMJHL')
-    print(url_list) #this is where I finished last
+    for team in url_list:
+        print(team)
 
 def get_roster_url(url_file, league):
     """Get the roster URLs for the specified league"""
     return_list = []
     with open(url_file, 'r') as rf:
         data = json.load(rf)
-        for team in data['Team Roster URLs'][league]: #go through the json and get the urls for each roster
-            return_list.append(data['Team Roster URLs'][league][team])
+        for team in data['Team Roster URLs'][league]:  # go through the json and get the urls for each roster
+            return_list.append((team, data['Team Roster URLs'][league][team]))  # add tuple of team name and url to list
     return return_list
 
 def main():
