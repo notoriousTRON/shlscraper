7f8397cb23642d70439d578022f3482503d816b0
MediocreFred <MediocreFred@users.noreply.github.com>
2020-01-26 17:20:25 -0500

    still trying to get it to work

--- scrape.py
+++ scrape.py
@@ -1,10 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-def get_smjhl_players(urls):
+def get_smjhl_players(url_file):
     """use the urls provided in the json to get each player's information"""
+    url_list = get_roster_url(url_file, 'SMJHL')
+    print(url_list) #this is where I finished last
+
+def get_roster_url(url_file, league):
+    """Get the roster URLs for the specified league"""
+    return_list = []
+    with open(url_file, 'r') as rf:
+        data = json.load(rf)
+        for team in data['Team Roster URLs'][league]: #go through the json and get the urls for each roster
+            return_list.append(data['Team Roster URLs'][league][team])
+    return return_list
 
 def main():
     """main"""
-    urls = "roster_urls.json"
-    get_smjhl_players(urls)
+    url_file = "roster_urls.json"
+    get_smjhl_players(url_file)
+
+main()
